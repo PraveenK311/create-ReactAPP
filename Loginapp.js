aad5932327d8e351ca48f50dee5eade824096d82
kumar311 <67825824+kumar311@users.noreply.github.com>
2020-12-14 12:00:51 +0530

    Add files via upload

---
+++ Loginapp.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+class Login extends React.Component {
+  constructor() {
+      super()
+      this.state = {
+          isLoggedIn: false
+      }
+      this.handleClick = this.handleClick.bind(this)
+  }
+  
+  handleClick() {
+      this.setState(prevState => {
+          return {
+              isLoggedIn: !prevState.isLoggedIn
+          }
+      })
+  }
+  
+  render() {   
+      let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
+      let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out"
+        return (
+            <div>
+                <button id="Signinbutton"onClick={this.handleClick}>{buttonText}</button>
+                <h2>{displayText}</h2>
+          </div>
+
+      )
+  }
+}
+
+export default Login;
